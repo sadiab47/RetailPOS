a0944a034d9c25f49634d302da1376d61699ec86
sadiab47 <81868595+sadiab47@users.noreply.github.com>
2026-07-20 17:39:26 +0500

    chore: add database setup and prisma schema

---
+++ backend/src/database.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class DatabaseService {
+  constructor() {
+    // Database service placeholder
+    // Will be configured to connect to MySQL retailpos database
+    console.log('Database service initialized');
+  }
+}
